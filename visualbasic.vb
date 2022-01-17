ffd19aede6ed65bbc9bde6ccd008d2add80e747e
Fadhil <67850930+fadhilsaheer@users.noreply.github.com>
2022-01-17 20:07:33 +0530

    Visual Basic

---
+++ visualbasic.vb
@@ -0,0 +1,11 @@
+Imports System 'System is a Namespace  
+Module Hello_Program  
+  
+    Sub Main()  
+  
+        Console.WriteLine("Hello World 👋")  
+        Console.ReadKey()  
+  
+    End Sub  
+  
+End Module  
